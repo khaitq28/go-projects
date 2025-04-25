3ba9eec9980d0558526ec3d5e7a19460945d8629
TRAN Quang Khai <canhcung88@gmail.com>
2025-04-25 20:04:12 +0200

    task..

--- todo-app/internal/task/service.go
+++ todo-app/internal/task/service.go
@@ -1,1 +1,28 @@
 package task
+
+type TaskService interface {
+	DeleteById(id uint) error
+	Toggle(id uint) error
+	Create(task *Task) error
+}
+
+type taskService struct {
+	taskRepository TaskRepository
+}
+
+func NewTaskService(repo TaskRepository) TaskService {
+	service := taskService{taskRepository: repo}
+	return &service
+}
+
+func (service *taskService) Create(task *Task) error {
+	return service.taskRepository.Create(task)
+}
+
+func (service *taskService) DeleteById(id uint) error {
+	return service.taskRepository.DeleteById(id)
+}
+
+func (service *taskService) Toggle(id uint) error {
+	return service.taskRepository.ToggleTask(id)
+}
